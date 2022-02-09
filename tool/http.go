6e408dfef2f85a742e7a2a52bfc424c555586a16
hellojukay <hellojukay@163.com>
2022-02-09 15:38:26 +0800

    Update http.go

--- tool/http.go
+++ tool/http.go
@@ -21,9 +21,6 @@ func init() {
 func Get(url string) (io.ReadCloser, error) {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
-	req.Header.Set("referer", "https://jable.tv/")
-	req.Header.Set("origin", "https://jable.tv/")
-
 	resp, err := c.Do(req)
 	if err != nil {
 		return nil, err
